c63dcf50aca189ee45387d2b9b5784bc9b65e40e
siddharth-bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2021-09-29 15:41:17 +0530

    clean up

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
+import './styles/App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>This is going to be an awesome app!</h1>
+    </div>
+  )
+}
+
+export default App;
