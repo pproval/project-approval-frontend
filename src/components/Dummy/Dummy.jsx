470c5b0a09f11f406bebe32826309378589cf3c6
siddharth-bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2021-09-30 13:03:50 +0530

    Logout btn added

--- src/components/Dummy/Dummy.jsx
+++ src/components/Dummy/Dummy.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../context/AuthProvider';
 
 function Dummy() {
+
+    const { logout } = useContext(AuthContext);
+
+    const handleSignout = async (e) => {
+        try {
+            await logout();
+        }
+        catch (err) {
+            console.log(err.message);
+        }
+    }
+
     return (
         <div>
             <h1>This is an awesome app!</h1>
+            <button onClick={handleSignout}>Log out</button>
         </div>
     )
 }
