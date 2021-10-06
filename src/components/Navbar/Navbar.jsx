9f5357945f10850fd6392fd6c3a7a6a06daf58fc
Shubham Bhardwaj <shubham200015@yahoo.com>
2021-10-06 21:05:53 +0530

    Add sidebar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState, useContext } from 'react';
 import { AuthContext } from '../../context/AuthProvider';
 import LinkButton from '../LinkButton/LinkButton';
 import Hamburger from '../Hamburger/Hamburger';
+import Sidebar from '../Sidebar/Sidebar';
 
 // Styles
 import './Navbar.css';
@@ -24,31 +25,38 @@ export default function Navbar() {
     }
 
     return (
-        <div className="Navbar">
+        <>
             {(currentUser === null) ?
-                <div className="Navbar-container">
-                    <div className="Navbar-logo">
-                        <h1>Pproval</h1>
+                <div className="Navbar">
+                    <div className="Navbar-container">
+                        <div className="Navbar-logo">
+                            <h1>Pproval</h1>
+                        </div>
+                        <ul className="Navbar-links">
+                            <li className="Navbar-link"><LinkButton content="About" routeLink="/home" special={false} /></li>
+                            <li className="Navbar-link"><LinkButton content="Login" routeLink="/login" special={false} /></li>
+                            <li className="Navbar-link"><LinkButton content="Register" routeLink="/register" special={true} /></li>
+                        </ul>
                     </div>
-                    <ul className="Navbar-links">
-                        <li className="Navbar-link"><LinkButton content="About" routeLink="/home" special={false} /></li>
-                        <li className="Navbar-link"><LinkButton content="Login" routeLink="/login" special={false} /></li>
-                        <li className="Navbar-link"><LinkButton content="Register" routeLink="/register" special={true} /></li>
-                    </ul>
                 </div>
                 :
-                <div className="Navbar-container">
-                    <div className="Navbar-logo">
-                        <div onClick={hamburgerClicked}>
-                            <Hamburger open={hamburgerState} />
+                <>
+                    <div className={hamburgerState ? "Navbar-open" : "Navbar"}>
+                        <div className="Navbar-container">
+                            <div className="Navbar-logo">
+                                <div onClick={hamburgerClicked}>
+                                    <Hamburger open={hamburgerState} />
+                                </div>
+                                <h1>Pproval</h1>
+                            </div>
+                            <ul className="Navbar-links-logout">
+                                <li className="Navbar-link"><a onClick={handleSignout} href="" className="Logout-button"><h3>Logout</h3></a></li>
+                            </ul>
                         </div>
-                        <h1>Pproval</h1>
                     </div>
-                    <ul className="Navbar-links-logout">
-                        <li className="Navbar-link"><a onClick={handleSignout} href="" className="Logout-button"><h3>Logout</h3></a></li>
-                    </ul>
-                </div>
+                    <Sidebar open={hamburgerState} />
+                </>
             }
-        </div>
+        </>
     )
 }
