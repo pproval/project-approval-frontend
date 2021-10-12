3444c6cbaa4d261f18cdb0e47ae0fdf0b65c15c1
Siddharth Bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2021-10-12 21:44:17 +0530

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,16 +5,4 @@ export const firebaseInit = {
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
-
-  apiKey: "AIzaSyBzFqyNpM-yK0fmlPGAJM_-DBpeYq6xxgg",
-
-  authDomain: "project-approval-system-e9ae2.firebaseapp.com",
-
-  projectId: "project-approval-system-e9ae2",
-
-  storageBucket: "project-approval-system-e9ae2.appspot.com",
-
-  messagingSenderId: "420836203928",
-
-  appId: "1:420836203928:web:f94ee32b1e8cbc93b6dfed",
 };
