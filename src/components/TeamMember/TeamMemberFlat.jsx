9e87ed64ae2677b1e8442a2b0c698e99cb35c5ef
Shubham Bhardwaj <shubham200015@yahoo.com>
2021-10-14 12:13:07 +0530

    Add basic content and styling to the project page

---
+++ src/components/TeamMember/TeamMemberFlat.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+// Image
+import Profile from '../Dashboard/Images/Profile.png'
+
+// Styles
+import './TeamMemberFlat.css'
+
+export default function TeamMemberFlat() {
+    return (
+        <div className="TeamMemberFlat">
+            <img src={Profile} alt="profile" className="TeamMemberFlat-Profile" />
+            <div style={{ textAlign: "center" }}>
+                <h3>FisrtName LastName</h3>
+            </div>
+        </div>
+    )
+}
