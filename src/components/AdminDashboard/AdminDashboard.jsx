4823fdad85893091a2c34ba8844f91398f775e96
siddharth-bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2021-10-16 11:30:07 +0530

    Lifting userData state up

--- src/components/AdminDashboard/AdminDashboard.jsx
+++ src/components/AdminDashboard/AdminDashboard.jsx
@@ -1,9 +1,6 @@
-import React, { useContext, useState, useEffect } from 'react';
-import { database } from '../../firebase/firebase';
-import { AuthContext } from '../../context/AuthProvider';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import Navbar from '../Navbar/Navbar'
-import LoadingScreen from '../LoadingScreen/LoadingScreen';
 
 // Images
 import Profile from '../StudentDashboard/Images/Profile.png'
@@ -11,47 +8,28 @@ import Profile from '../StudentDashboard/Images/Profile.png'
 // Styles
 import './AdminDashboard.css'
 
-export default function AdminDashboard() {
-    const { currentUser } = useContext(AuthContext);
-    // const [loading, setLoading] = useState(false);
-    const [userData, setUserData] = useState();
-
-    useEffect(() => {
-        // Attaching a listener on the current user's document in DB
-        // Whenever there is change/update, callback is fired and it updates userdata state
-        const unsubscribe = database.users.doc(currentUser.uid).onSnapshot((doc) => {
-            setUserData(doc.data());
-        });
-        return unsubscribe;
-    }, [currentUser]);
-
-    useEffect(() => {
-        console.log(userData);
-    })
-
+export default function AdminDashboard({ userData }) {
     return (
+
         <>
-            {userData == null ? <LoadingScreen /> :
-                <>
-                    <Navbar role={3} />
-                    <div className="StudentDashboard">
-                        <div className="StudentDashboard-container">
-                            <div className="StudentDashboard-header">
-                                <img src={Profile} alt="profile" className="profile-img" />
-                                <div className="StudentDashboard-metadata">
-                                    <h1 style={{ fontSize: "40px" }}>{userData.username}</h1>
-                                    <h3 style={{ color: "rgba(4, 255, 171, 1)" }}>Admin </h3>
-                                </div>
-                            </div>
-                            <div className="TeacherDashboard-body">
-                                <div className="TeacherDashboard-body-container">
-                                    <h2 className="TeacherDashboard-body-title">Users</h2>
-                                </div>
-                            </div>
+            <Navbar role={3} />
+            <div className="StudentDashboard">
+                <div className="StudentDashboard-container">
+                    <div className="StudentDashboard-header">
+                        <img src={Profile} alt="profile" className="profile-img" />
+                        <div className="StudentDashboard-metadata">
+                            <h1 style={{ fontSize: "40px" }}>{userData?.username}</h1>
+                            <h3 style={{ color: "rgba(4, 255, 171, 1)" }}>Admin </h3>
                         </div>
                     </div>
-                </>
-            }
+                    <div className="TeacherDashboard-body">
+                        <div className="TeacherDashboard-body-container">
+                            <h2 className="TeacherDashboard-body-title">Users</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
+
     )
 }
