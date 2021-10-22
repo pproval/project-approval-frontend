8376808e63a891a7131fc7d5c6c299d91bd923d6
siddharth-bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2021-10-23 00:22:39 +0530

    [feat] progress report, final report: upload, view

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -32,15 +32,26 @@ export default function Project() {
         }
     }
 
-    const handleUpload = async (e) => {
+    const handleUpload = async (e, code) => {
         e.preventDefault();
         try {
             setLoading(true);
             let pid = projectData?.pid;
             console.log(pid);
+            let uploadTaskListener;
 
-            // Creates projects/pid directories and stores file with synopsis name
-            const uploadTaskListener = storage.ref(`/projects/${pid}/synopsis`).put(file);
+            if (code === 0 || code === 1) {
+                // Creates projects/pid directories and stores file with synopsis name
+                uploadTaskListener = storage.ref(`/projects/${pid}/synopsis`).put(file);
+            }
+            else if (code === 2) {
+                // Creates/updates projects/pid directories and stores file with progress-report name
+                uploadTaskListener = storage.ref(`/projects/${pid}/progress-report`).put(file);
+            }
+            else if (code === 3) {
+                uploadTaskListener = storage.ref(`/projects/${pid}/final-report`).put(file);
+            }
+
             // Register three observers:
             // 1. 'state_changed' observer, called any time the state changes -- tracks progress
             // 2. Error observer, called on failure -- error handler
@@ -63,12 +74,29 @@ export default function Project() {
                 // link of synopsis just stored
                 let downloadURL = await uploadTaskListener.snapshot.ref.getDownloadURL();
                 console.log(downloadURL);
-                await database.projects.doc(pid).update({
-                    synopsis: downloadURL,
-                    status: 1,
-                });
+                if (code === 0 || code === 1) {
+                    await database.projects.doc(pid).update({
+                        synopsis: downloadURL,
+                        status: 1,
+                    });
+                    console.log("Synopsis uploaded");
+                }
+                else if (code === 2) {
+                    await database.projects.doc(pid).update({
+                        progressReport: downloadURL,
+                        status: 3,
+                    });
+                    console.log("Progress Report uploaded");
+                }
+                else if (code === 3) {
+                    await database.projects.doc(pid).update({
+                        finalReport: downloadURL,
+                        status: 4,
+                    });
+                    console.log("Progress Report uploaded");
+                }
+
                 setLoading(false);
-                console.log("Synopsis uploaded");
                 history.push('/project');
             }
         }
@@ -79,9 +107,9 @@ export default function Project() {
         }
     }
 
-    const viewFile = (e, synopsis) => {
-        if (synopsis !== null && synopsis !== undefined) {
-            window.open(synopsis);
+    const viewFile = (e, URL) => {
+        if (URL !== null && URL !== undefined) {
+            window.open(URL);
         }
     }
 
@@ -127,7 +155,7 @@ export default function Project() {
                     <h2>Synopsis</h2>
                     <div className="Project-doc-box">
                         <h3>Minor Project Synopsis-Project Approval System</h3>
-                        <a href="" className="Project-doc-view"><h3>View</h3></a>
+                        <button onClick={(e) => viewFile(e, projectData?.synopsis)} className="Project-doc-view"><h3>View</h3></button>
                     </div>
                 </div>
             )
@@ -142,9 +170,9 @@ export default function Project() {
                     <h2>Upload Progress Report</h2>
                     <div className="Project-doc-box">
                         <form className="Project-doc-form">
-                            <input type="file" className="Project-doc-input"></input>
+                            <input type="file" onChange={(e) => handleFileUpload(e)} className="Project-doc-input"></input>
                         </form>
-                        <a href="" className="Project-doc-view"><h3>Upload</h3></a>
+                        <button disabled={file === null ? true : file.type === 'application/pdf' ? false : true} onClick={(e) => handleUpload(e, 2)} className="Project-doc-view"><h3>Upload</h3></button>
                     </div>
                 </div>
             )
@@ -157,7 +185,7 @@ export default function Project() {
                     <h2>Progress Report</h2>
                     <div className="Project-doc-box">
                         <h3>Minor Project Progress Report-Project Approval System</h3>
-                        <a href="" className="Project-doc-view"><h3>View</h3></a>
+                        <button onClick={(e) => viewFile(e, projectData?.progressReport)} className="Project-doc-view"><h3>View</h3></button>
                     </div>
                 </div>
             )
@@ -172,9 +200,9 @@ export default function Project() {
                     <h2>Upload Final Report</h2>
                     <div className="Project-doc-box">
                         <form>
-                            <input type="file" className="Project-doc-input"></input>
+                            <input type="file" onChange={(e) => handleFileUpload(e)} className="Project-doc-input"></input>
                         </form>
-                        <a href="" className="Project-doc-view"><h3>Upload</h3></a>
+                        <button disabled={file === null ? true : file.type === 'application/pdf' ? false : true} onClick={(e) => handleUpload(e, 3)} className="Project-doc-view"><h3>Upload</h3></button>
                     </div>
                 </div>
             )
@@ -187,7 +215,7 @@ export default function Project() {
                     <h2>Final Report</h2>
                     <div className="Project-doc-box">
                         <h3>Final Report-Project Approval System</h3>
-                        <a href="" className="Project-doc-view"><h3>View</h3></a>
+                        <button onClick={(e) => viewFile(e, projectData?.finalReport)} className="Project-doc-view"><h3>View</h3></button>
                     </div>
                 </div>
             )
